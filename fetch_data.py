1b664ea1d6a99c5f3f35bdc276ce89264639189a
ebru93-aylin <linfelya.sky@gmail.com>
2025-07-04 16:15:35 +0300

    Delete fetch_data.py

--- fetch_data.py
+++
@@ -1,7 +0,0 @@
-
-import requests
-
-def fetch_coin_data(coin_id):
-    url = f"https://api.coingecko.com/api/v3/coins/{coin_id}?localization=false"
-    response = requests.get(url)
-    return response.json()
