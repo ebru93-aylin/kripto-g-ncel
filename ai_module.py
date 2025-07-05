ceb24a3dbcd6a98a87a76990169734851f2216ad
ebru93-aylin <linfelya.sky@gmail.com>
2025-07-05 15:25:26 +0300

    Delete ai_module.py

--- ai_module.py
+++
@@ -1,8 +0,0 @@
-
-def ai_signal(rsi, macd, ao, wavetrend):
-    if rsi > 70 and macd == "SELL" and ao < 0:
-        return "SAT", "Yüksek Risk"
-    elif rsi < 30 and macd == "BUY" and ao > 0:
-        return "AL", "Düşük Risk"
-    else:
-        return "BEKLE", "Orta Risk"
